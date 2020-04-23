676a1360c7057395b46806b339d08b358a9bdb49
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2020-04-23 16:55:57 +0300

    react-native-splash-screen.patch

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -12,6 +12,8 @@
 
 static bool waiting = true;
 static bool addedJsLoadErrorObserver = false;
+static UIView* gRootView = nil;
+static UIView* loadingView = nil;
 
 @implementation RNSplashScreen
 - (dispatch_queue_t)methodQueue{
@@ -29,13 +31,35 @@ RCT_EXPORT_MODULE(SplashScreen)
         NSDate* later = [NSDate dateWithTimeIntervalSinceNow:0.1];
         [[NSRunLoop mainRunLoop] runUntilDate:later];
     }
+
+    if (loadingView) {
+        [gRootView addSubview:loadingView];
+    }
+}
+
++ (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView {
+    if (!loadingView) {
+        gRootView = rootView;
+        loadingView = [[[NSBundle mainBundle] loadNibNamed:splashScreen owner:self options:nil] objectAtIndex:0];
+        CGRect frame = rootView.frame;
+        frame.origin = CGPointMake(0, 0);
+        loadingView.frame = frame;
+    }
+    waiting = false;
+
+    [rootView addSubview:loadingView];
 }
 
 + (void)hide {
-    dispatch_async(dispatch_get_main_queue(),
-                   ^{
-                       waiting = false;
-                   });
+    if (waiting) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            waiting = false;
+        });
+    } else {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [loadingView removeFromSuperview];
+        });
+    }
 }
 
 + (void) jsLoadError:(NSNotification*)notification
